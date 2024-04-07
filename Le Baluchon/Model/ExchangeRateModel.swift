bfdcddb94f6aff31c1d15b99e5d73382f7cf3ad5
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2024-04-07 05:08:50 +0200

    aajout documentation

--- Le Baluchon/Model/ExchangeRateModel.swift
+++ Le Baluchon/Model/ExchangeRateModel.swift
@@ -35,9 +35,9 @@ extension ExchangeRate {
         self.date = date
     }
     
-    /// <#Description#>
-    /// - Parameter amount: <#amount description#>
-    /// - Returns: <#description#>
+    /// Converts an amount from the base currency to the target currency using the exchange rate.
+    /// - Parameter amount: The amount in the base currency to be converted.
+    /// - Returns: The equivalent amount in the target currency, based on the current exchange rate.
     func convert(amount: Double) -> Double {
         return amount * rate
     }
